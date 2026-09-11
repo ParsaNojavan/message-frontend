c6887d7b8da6f2632cfcd789af0f6ca20a72d06b
parsanojavan <parsa.nojavan85@gmail.com>
2026-09-11 16:29:48 +0330

    feat(chat): add message context menu

--- src/components/chat/window/chat-window.component.ts
+++ src/components/chat/window/chat-window.component.ts
@@ -3,22 +3,28 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../../../services/chat/chat.service';
 import { provideIcons, NgIconComponent } from '@ng-icons/core';
-import { 
-  lucidePhone, 
-  lucideVideo, 
-  lucideSearch, 
-  lucideMoreVertical, 
-  lucideSend, 
-  lucidePaperclip, 
-  lucideSmile, 
+import {
+  lucidePhone,
+  lucideVideo,
+  lucideSearch,
+  lucideMoreVertical,
+  lucideSend,
+  lucidePaperclip,
+  lucideSmile,
   lucideCheckCheck,
   lucidePin,
-  lucideReply
+  lucideReply,
+  lucideCopy,
+  lucideForward,
+  lucidePencil,
+  lucideTrash2
 } from '@ng-icons/lucide';
 
 // Spartan UI New Imports
 import { HlmAvatarImports } from '@spartan-ng/helm/avatar';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmContextMenuImports } from '@spartan-ng/helm/context-menu';
+import { HlmDropdownMenuImports } from '@spartan-ng/helm/dropdown-menu';
 
 @Component({
   selector: 'app-chat-window',
@@ -28,7 +34,9 @@ import { HlmButtonImports } from '@spartan-ng/helm/button';
     FormsModule,
     NgIconComponent,
     HlmAvatarImports,
-    HlmButtonImports
+    HlmButtonImports,
+    HlmContextMenuImports,
+    HlmDropdownMenuImports
   ],
   providers: [
     provideIcons({
@@ -41,7 +49,11 @@ import { HlmButtonImports } from '@spartan-ng/helm/button';
       lucideSmile,
       lucideCheckCheck,
       lucidePin,
-      lucideReply
+      lucideReply,
+      lucideCopy,
+      lucideForward,
+      lucidePencil,
+      lucideTrash2
     })
   ],
   templateUrl: './chat-window.component.html'
@@ -64,6 +76,44 @@ export class ChatWindowComponent implements AfterViewChecked {
   private scrollToBottom(): void {
     if (this.scrollContainer) {
       this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+    }
+  }
+
+  onReply(msg: any) {
+    console.log('Reply to message:', msg);
+  }
+
+  onCopy(msg: any) {
+    if (msg.content) {
+      navigator.clipboard.writeText(msg.content);
+    }
+  }
+
+  onForward(msg: any) {
+    console.log('Forward message:', msg);
+  }
+
+  onPin(msg: any) {
+    console.log('Pin message:', msg);
+  }
+
+  onEdit(msg: any) {
+    console.log('Edit message:', msg);
+  }
+
+  onDelete(msg: any) {
+    console.log('Delete message:', msg);
+  }
+
+  onReaction(msg: any, emoji: string) {
+    console.log('Reaction:', emoji, 'on message:', msg.id);
+  }
+
+  onEmojiWheel(event: WheelEvent): void {
+    const container = event.currentTarget as HTMLElement;
+    if (container) {
+      event.preventDefault();
+      container.scrollLeft += event.deltaY;
     }
   }
 }
